6ff17d1ee3db9e1a6ded3bda16e776a6d4521262
surajrocks1997 <surajrocks1997@gmail.com>
2020-08-27 17:51:57 +0530

    user signup added: backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,25 @@
-const express = require('express')
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
 
-})
+  try {
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      hashPassword,
+    });
+
+    const result = await user.save();
+    res.status(201).json({ message: "User Created", result });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 module.exports = router;
